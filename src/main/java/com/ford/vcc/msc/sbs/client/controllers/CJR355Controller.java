531c9025cf647bd38361ea030b4f14b6a6699186
Sattar <ASATTAR@volvocars.com>
2020-07-24 15:30:31 +0530

    second commit

--- src/main/java/com/ford/vcc/msc/sbs/client/controllers/CJR355Controller.java
+++ src/main/java/com/ford/vcc/msc/sbs/client/controllers/CJR355Controller.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +20,8 @@ import com.ford.vcc.msc.sbs.client.services.CJR355Service;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
-
-@RestController
+   
+@RestController       
 @RequestMapping("/SBSToolBox")
 @Api(value = "Operations pertaining to SBSToolBox CJR355", description = "Operations pertaining to SBSToolBox CJR355", tags = {
 		"SBSToolBox APIs" })
